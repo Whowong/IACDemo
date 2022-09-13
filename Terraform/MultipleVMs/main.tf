6888023c67560a11472ff1d46868693e9fbcaeee
Whowong <54148527+Whowong@users.noreply.github.com>
2022-09-13 14:31:28 -0700

    Updating to create more vms with a backup

--- Terraform/MultipleVMs/main.tf
+++ Terraform/MultipleVMs/main.tf
@@ -1,25 +1,26 @@
 provider "azurerm" {
-  version = "= 2.0.0"
+  version = "= 2.99"
   features {}
+  use_msal = false
 }
 
 variable "numInstances" {
   type        = number
-  default     = 2
+  default     = 25
   description = "Number of VMs"
 }
 
 terraform{
   backend "azurerm" {
   resource_group_name  = "AH_SharedServices"
-  storage_account_name = "ahcsaterraform"
+  storage_account_name = "ahcsaiacterraform"
   container_name       = "terraform"
   key                  = "multiplevms.tfstate"
  }  
 }
 
 data "azurerm_key_vault" "akv" {
-  name                = "IACDemo"
+  name                = "AHCSAIACDemo"
   resource_group_name = "AH_SharedServices"
 }
 
@@ -30,13 +31,13 @@ data "azurerm_key_vault_secret" "vmadminpassword" {
 
 resource "azurerm_resource_group" "rg" {
   name = "rg-IACDemoTFMultipleVMs"
-  location = "westus2"
+  location = "eastus2"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name = "vnet-IACDemoTFMultipleVMs"
   address_space = ["10.0.0.0/16"]
-  location = "westus2"
+  location = "eastus2"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -44,14 +45,14 @@ resource "azurerm_subnet" "frontendsubnet" {
   name = "snet-IACDemoTFMultipleVMsFrontend"
   resource_group_name =  azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix = "10.0.1.0/24"
+  address_prefixes = ["10.0.1.0/24"]
 }
 
 
 resource "azurerm_network_interface" "vm1nic" {
   count = var.numInstances
   name = "nic-IACDemoTFMultipleVMs${count.index}"
-  location = "westus2"
+  location = "eastus2"
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
@@ -61,10 +62,44 @@ resource "azurerm_network_interface" "vm1nic" {
   }
 }
 
+resource "azurerm_recovery_services_vault" "recoveryvault" {
+  name                = "tfex-recovery-vault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  soft_delete_enabled = false
+}
+
+resource "azurerm_backup_policy_vm" "backuppolicy" {
+
+  name                = "tfex-recovery-vault-policy"
+  resource_group_name = azurerm_resource_group.rg.name
+  recovery_vault_name = azurerm_recovery_services_vault.recoveryvault.name
+
+  backup {
+    frequency = "Daily"
+    time      = "23:00"
+  }
+
+  retention_daily {
+    count = 10
+  }
+}
+
+resource "azurerm_backup_protected_vm" "vm1" {
+  count               = var.numInstances
+  resource_group_name = azurerm_resource_group.rg.name
+  recovery_vault_name = azurerm_recovery_services_vault.recoveryvault.name
+  source_vm_id        = azurerm_linux_virtual_machine.vm[count.index].id
+  backup_policy_id    = azurerm_backup_policy_vm.backuppolicy.id
+  depends_on = [
+    azurerm_linux_virtual_machine.vm
+  ]
+}
 resource "azurerm_linux_virtual_machine" "vm" {
   count                 = var.numInstances
   name                  = "vm-IACDemoTFMultipleVMs${count.index}"  
-  location              = "westus2"
+  location              = "eastus2"
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [element(azurerm_network_interface.vm1nic.*.id, count.index)]
   size                  = "Standard_F2"
